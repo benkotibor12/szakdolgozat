a4705a8701204af3f0321d9e703e384dbaf6449d
Benkő Tibor <btiborb98@gmail.com>
2023-04-21 09:58:59 +0200

    Game Settings - Creating new options in main menu - Adding a new appearance for the main menu

---
+++ Assets/SettingsController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour
+{
+    public void ApplySettings()
+    {
+        Debug.Log("Save");
+    }
+}
