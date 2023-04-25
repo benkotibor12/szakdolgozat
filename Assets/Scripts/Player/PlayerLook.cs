f5f0178ade0943bfc8617ad3c587a7f2d5fb9748
Benkő Tibor <btiborb98@gmail.com>
2023-04-25 11:49:47 +0200

    Game Settings - Adding new fields to GameData.cs - Creating options UI and scripts to change resolution, graphics quality, fullscreen mode, sound and mouse sensitivity - Saving these changes to data.json with the modified GameData.cs

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -1,22 +1,34 @@
 using UnityEngine;
 
-public class PlayerLook : MonoBehaviour
+public class PlayerLook : MonoBehaviour, IDataPersistence
 {
     public Camera playerCamera;
     private float rotationX = 0f;
     public float rotationZ = 0;
     public float rotationY = 0;
-    public float sensitivityX = 30f;
-    public float sensitivityY = 30f;
+    public float horizontalSensitivity = 30f;
+    public float verticalSensitivity = 30f;
+
 
     public void ProcessLook(Vector2 input)
     {
         float mouseX = input.x;
         float mouseY = input.y;
 
-        rotationX -= (mouseY * Time.deltaTime) * sensitivityY;
-        rotationX = Mathf.Clamp(rotationX, -80f, 80f);
+        rotationX -= (mouseY * Time.deltaTime) * horizontalSensitivity;
+        rotationX = Mathf.Clamp(rotationX, -80.0f, 80.0f);
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, rotationZ);
-        transform.Rotate((mouseX * Time.deltaTime) * sensitivityX * Vector3.up);
+        transform.Rotate((mouseX * Time.deltaTime) * verticalSensitivity * Vector3.up);
+    }
+
+    public void LoadData(GameData gameData)
+    {
+        horizontalSensitivity = gameData.horizontalSensitivity;
+        verticalSensitivity = gameData.verticalSensitivity;
+    }
+
+    public void SaveData(ref GameData gameData)
+    {
+        //
     }
 }
--- Assets/SettingsController.cs
+++ Assets/SettingsController.cs
@@ -1,11 +1,123 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
-public class SettingsController : MonoBehaviour
+public class SettingsController : MonoBehaviour, IDataPersistence
 {
+    [SerializeField] private TextMeshProUGUI volumeTextValue;
+    [SerializeField] private Slider volumeSlider;
+
+    [SerializeField] private TextMeshProUGUI verticalSensitivityTextValue;
+    [SerializeField] private Slider verticalSensitivitySlider;
+    [SerializeField] private TextMeshProUGUI horizontalSensitivityTextValue;
+    [SerializeField] private Slider horizontalSensitivitySlider;
+
+    [SerializeField] private TextMeshProUGUI brightnessTextValue;
+    [SerializeField] private Slider brightnessSlider;
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private float audioVolume;
+    private float horizontalSensitivity;
+    private float verticalSensitivity;
+
+    private int quailityValue;
+    private float brightnessValue;
+    private bool isFullScreen = true;
+    private int resolution;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolution = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolution = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolution;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void ApplySettings()
     {
-        Debug.Log("Save");
+        DataPersistenceManager.Instance.SaveGame();
+    }
+
+    public void SetVerticalSensitivity(float sensitivity)
+    {
+        verticalSensitivity = sensitivity;
+        verticalSensitivityTextValue.text = sensitivity.ToString("0");
+    }
+
+    public void SetHorizontalSensitivity(float sensitivity)
+    {
+        horizontalSensitivity = sensitivity;
+        horizontalSensitivityTextValue.text = sensitivity.ToString("0");
+
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioVolume = volume;
+        volumeTextValue.text = volume.ToString("0");
+        AudioListener.volume = audioVolume;
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        quailityValue = qualityIndex;
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetBrightness(float brightness)
+    {
+        brightnessValue = brightness;
+        brightnessTextValue.text = brightness.ToString("0");
+    }
+
+    public void SetFullScreen(bool fullscreen)
+    {
+        isFullScreen = fullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        resolution = resolutionIndex;
+        Screen.SetResolution(resolutions[resolution].width, resolutions[resolution].height, isFullScreen);
+    }
+
+    public void LoadData(GameData gameData)
+    {
+        volumeTextValue.text = gameData.audioVolume.ToString("0");
+        volumeSlider.value = gameData.audioVolume;
+        verticalSensitivityTextValue.text = gameData.verticalSensitivity.ToString("0");
+        verticalSensitivitySlider.value = gameData.verticalSensitivity;
+        horizontalSensitivityTextValue.text = gameData.horizontalSensitivity.ToString("0");
+        horizontalSensitivitySlider.value = gameData.horizontalSensitivity;
+    }
+
+    public void SaveData(ref GameData gameData)
+    {
+        gameData.audioVolume = audioVolume;
+        gameData.verticalSensitivity = verticalSensitivity;
+        gameData.horizontalSensitivity = horizontalSensitivity;
+        gameData.brightness = brightnessValue;
+        gameData.isFullScreen = isFullScreen;
+        gameData.quaility = quailityValue;
+        gameData.resolution = resolution;
     }
 }
